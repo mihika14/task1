adb0df5c094a209302378f2c06ae25d23da0175b
Mihika Saxena <mihikasaxena13@gmail.com>
2024-03-10 18:32:28 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Resizable } from "re-resizable";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import AreaChart from "./components/AreaChart";
+import ColumnChart from "./components/ColumnChart";
 
-function App() {
+const App = () => {
+  const [box1Width, setBox1Width] = useState(800);
+  const [box1Height, setBox1Height] = useState(400);
+  const [box2Width, setBox2Width] = useState(700);
+  const [box2Height, setBox2Height] = useState(400);
+  const [box3Width, setBox3Width] = useState(1000);
+  const [box3Height, setBox3Height] = useState(200);
+
+  const handleResizeBox1 = (e, direction, ref, d) => {
+    setBox1Width(box1Width + d.width);
+    setBox1Height(box1Height + d.height);
+    setBox2Width(box2Width - d.width);
+    setBox2Height(box2Height + d.height);
+  };
+
+  const handleResizeBox2 = (e, direction, ref, d) => {
+    setBox1Width(box1Width - d.width);
+    setBox1Height(box1Height + d.height);
+    setBox2Width(box2Width + d.width);
+    setBox2Height(box2Height + d.height);
+  };
+
+  const handleResizeBox3 = (e, direction, ref, d) => {
+    setBox3Width(box3Width + d.width);
+    setBox3Height(box3Height + d.height);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <h1 className="title">Dashboard</h1>
+        <div className="upper-part">
+          <div className="box">
+            <Resizable
+              size={{ width: box1Width, height: box1Height }}
+              onResizeStop={handleResizeBox1}
+            >
+              <BarChart />
+            </Resizable>
+          </div>
+          <div className="box">
+            <Resizable
+              size={{ width: box2Width, height: box2Height }}
+              onResizeStop={handleResizeBox2}
+            >
+              <AreaChart />
+            </Resizable>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-part">
+        <Resizable
+          size={{ width: box3Width, height: box3Height }}
+          onResizeStop={handleResizeBox3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <h1 className="title">This is Task 1 of the assignment. </h1>
+            <p className="task-description">
+              <ul>
+                <li className="list-item">
+                  Purpose: The app serves as a dashboard interface, providing
+                  users with visual representations of data through various
+                  types of charts.
+                </li>
+
+                <li>
+                  Technologies Used: <br/>
+                  React: The app is built using React, a
+                  JavaScript library for building user interfaces. re-resizable:
+                  This library is utilized to create resizable components,
+                  allowing users to adjust the size of the chart containers
+                  dynamically. CSS: Styling is applied to create an
+                  aesthetically pleasing layout and appearance for the
+                  dashboard.
+                </li>
+
+                <li>
+                  This combination allows for a more user-friendly experience
+                  and enhances the overall interactivity of the application.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </Resizable>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/AreaChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2013", 1000, 400],
+  ["2014", 1170, 460],
+  ["2015", 660, 1120],
+  ["2016", 1030, 540],
+];
+const options = {
+  title: "Company Performance",
+  hAxis: { title: "Year", titleTextStyle: { color: "#333" } },
+  vAxis: { minValue: 0 },
+  chartArea: { width: "50%", height: "70%" },
+};
+
+export default function AreaChart() {
+  return (
+    <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
